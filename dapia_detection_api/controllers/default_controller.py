19e4d1f640a5965648c2093eeabd652f78775f5c
Morgan LECLERCQ <morgan.leclercq89@gmail.com>
2023-10-30 16:05:00 +0100

    modify default_controller_api

--- dapia_detection_api/controllers/default_controller.py
+++ dapia_detection_api/controllers/default_controller.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from AircraftClassifier import predictAircraftType, probabilityToLabel, labelToName, CONTEXT
+from AdsbAnomalyDetector import predictAircraftType, probabilityToLabel, labelToName, getTruthLabelFromIcao
 
 from dapia_detection_api.types.fields import AdsbMessageField
 
@@ -23,47 +23,14 @@ def classify_aircraft(body):  # noqa: E501
     :rtype: Union[SendMessagePost200Response, Tuple[SendMessagePost200Response, int], Tuple[SendMessagePost200Response, int, Dict[str, str]]
     """
     message = body["message"]
+    print(message)
     icao = message[AdsbMessageField.ICAO]
 
-    if icao not in message_by_icao:
-        message_by_icao[icao] = []
-    message_by_icao[icao].append(message)
-    messages = message_by_icao[icao][-CONTEXT.HISTORY:]
+    predictions = {}
+    predictions[icao] = []
 
-    if len(messages) == CONTEXT.HISTORY:
-        df = (pd.DataFrame(messages)
-              .replace('', np.NaN)
-              .fillna(method='ffill')
-              .fillna(method='bfill')
-              .fillna(value=0))
-
-        try:
-            probability = predictAircraftType(
-                np.array([np.int64(value) for value in df[AdsbMessageField.TIMESTAMP]]).reshape([1, 128]),
-                np.array([np.float64(value) for value in df[AdsbMessageField.LATITUDE]]).reshape([1, 128]),
-                np.array([np.float64(value) for value in df[AdsbMessageField.LONGITUDE]]).reshape([1, 128]),
-                np.array([np.float64(value) for value in df[AdsbMessageField.GROUND_SPEED]]).reshape([1, 128]),
-                np.array([np.float64(value) for value in df[AdsbMessageField.TRACK]]).reshape([1, 128]),
-                np.array([np.float64(value) for value in df[AdsbMessageField.VERTICAL_RATE]]).reshape([1, 128]),
-                np.array([np.bool_(value) for value in df[AdsbMessageField.ON_GROUND]]).reshape([1, 128]),
-                np.array([np.bool_(value) for value in df[AdsbMessageField.ALERT]]).reshape([1, 128]),
-                np.array([np.bool_(value) for value in df[AdsbMessageField.SPI]]).reshape([1, 128]),
-                np.array([np.int64(value) for value in df[AdsbMessageField.SQUAWK]]).reshape([1, 128]),
-                np.array([np.float64(value) for value in df[AdsbMessageField.ALTITUDE]]).reshape([1, 128]),
-                np.array([np.float64(value) for value in df[AdsbMessageField.GEO_ALTITUDE]]).reshape([1, 128])
-            )
-            response = {'message': message, 'prediction': probability_to_name(probability)}
-        except Exception as e:
-            response = {'message': message, 'error': f'{e}'}, 500
-
-        return response
-    else:
-        return {'message': message, 'prediction': "Not available"}
-
-
-def probability_to_name(probability):
-    label = probabilityToLabel(probability)
-    name = labelToName(label)
-    if len(name) > 0:
-        return name[0]
-    return 'Unavailable'
+    a = predictAircraftType([message])
+    for icao, proba in a.items():
+        predictions[icao].append(proba)
+    print("done")
+    print(predictions)
