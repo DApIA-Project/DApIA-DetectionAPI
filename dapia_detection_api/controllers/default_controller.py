01c8efc8f3c723729316682156cec416f314a594
Morgan LECLERCQ <morgan.leclercq89@gmail.com>
2023-10-31 16:25:59 +0100

    problems dependencies and modify detection api

--- dapia_detection_api/controllers/default_controller.py
+++ dapia_detection_api/controllers/default_controller.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from AdsbAnomalyDetector import predictAircraftType, probabilityToLabel, labelToName, getTruthLabelFromIcao
 
 from dapia_detection_api.types.fields import AdsbMessageField
@@ -22,15 +21,53 @@ def classify_aircraft(body):  # noqa: E501
 
     :rtype: Union[SendMessagePost200Response, Tuple[SendMessagePost200Response, int], Tuple[SendMessagePost200Response, int, Dict[str, str]]
     """
+    orginalMessage = body["message"]
     message = body["message"]
-    print(message)
-    icao = message[AdsbMessageField.ICAO]
 
-    predictions = {}
-    predictions[icao] = []
+    try:
+        icao = message[AdsbMessageField.ICAO]
 
-    a = predictAircraftType([message])
-    for icao, proba in a.items():
-        predictions[icao].append(proba)
-    print("done")
-    print(predictions)
+        predictions = {}
+        predictions[icao] = []
+        message[AdsbMessageField.TIMESTAMP] = int(orginalMessage[AdsbMessageField.TIMESTAMP])
+        message[AdsbMessageField.LATITUDE] = float(orginalMessage[AdsbMessageField.LATITUDE])
+        message[AdsbMessageField.LONGITUDE] = float(orginalMessage[AdsbMessageField.LONGITUDE])
+        message[AdsbMessageField.GROUND_SPEED] = float(orginalMessage[AdsbMessageField.GROUND_SPEED])
+        message[AdsbMessageField.TRACK] = float(orginalMessage[AdsbMessageField.TRACK])
+        message[AdsbMessageField.VERTICAL_RATE] = float(orginalMessage[AdsbMessageField.VERTICAL_RATE])
+
+        message[AdsbMessageField.ALTITUDE] = float(orginalMessage[AdsbMessageField.ALTITUDE])
+        message[AdsbMessageField.GEO_ALTITUDE] = float(orginalMessage[AdsbMessageField.GEO_ALTITUDE])
+
+        if message[AdsbMessageField.SQUAWK] != "NaN":
+            message[AdsbMessageField.SQUAWK] = int(orginalMessage[AdsbMessageField.SQUAWK])
+        else:
+            message[AdsbMessageField.SQUAWK] = None
+
+        if message[AdsbMessageField.ON_GROUND] == "True":
+            message[AdsbMessageField.ON_GROUND] = True
+        else:
+            message[AdsbMessageField.ON_GROUND] = False
+
+        if message[AdsbMessageField.ALERT] == "True":
+            message[AdsbMessageField.ALERT] = True
+        else:
+            message[AdsbMessageField.ALERT] = False
+
+        if message[AdsbMessageField.SPI] == "True":
+            message[AdsbMessageField.SPI] = True
+        else:
+            message[AdsbMessageField.SPI] = False
+
+        a = predictAircraftType([message])
+        for icao, proba in a.items():
+            predictions[icao].append(proba)
+
+        labels_flight_1 = probabilityToLabel(predictions[icao])
+        major_label_flight_1 = np.bincount(labels_flight_1).argmax()
+        truth = getTruthLabelFromIcao(icao)
+
+        return {'message': orginalMessage, 'prediction': labelToName(major_label_flight_1),
+                'truth': labelToName(truth)}, 200
+    except Exception as e:
+        return {'message': orginalMessage, 'error': f'{e}'}, 500
--- dapia_detection_api/models/__init__.py
+++ dapia_detection_api/models/__init__.py
@@ -1,3 +1,6 @@
 # flake8: noqa
 # import models into model package
-from dapia_detection_api.models.adsb_message import AdsbMessage
+from dapia_detection_api.models.aircraft_types import AircraftTypes
+from dapia_detection_api.models.classify_aircraft_request import ClassifyAircraftRequest
+from dapia_detection_api.models.data import Data
+from dapia_detection_api.models.response200_classify_aircraft import Response200ClassifyAircraft
