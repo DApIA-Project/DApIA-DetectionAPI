746f73ce475db4022912e8fe545cfc1cff891542
Morgan LECLERCQ <morgan.leclercq89@gmail.com>
2023-11-08 15:43:28 +0100

    fix : no modify the original message

--- dapia_detection_api/controllers/default_controller.py
+++ dapia_detection_api/controllers/default_controller.py
@@ -1,10 +1,11 @@
+import copy
+
 import numpy as np
 from AdsbAnomalyDetector import predictAircraftType, probabilityToLabel, labelToName, getTruthLabelFromIcao
 
 from dapia_detection_api.types.fields import AdsbMessageField
 
 message_by_icao = {}
-
 
 
 def classify_aircraft(body):  # noqa: E501
@@ -17,28 +18,51 @@ def classify_aircraft(body):  # noqa: E501
 
     :rtype: Union[SendMessagePost200Response, Tuple[SendMessagePost200Response, int], Tuple[SendMessagePost200Response, int, Dict[str, str]]
     """
-    orginalMessage = body["message"]
-    message = body["message"]
 
+    originalMessage = copy.copy(body["message"])
+    message = copy.copy(body["message"])
     try:
         icao = message[AdsbMessageField.ICAO]
 
         predictions = {}
         predictions[icao] = []
-        message[AdsbMessageField.TIMESTAMP] = int(orginalMessage[AdsbMessageField.TIMESTAMP])
-        message[AdsbMessageField.LATITUDE] = float(orginalMessage[AdsbMessageField.LATITUDE])
-        message[AdsbMessageField.LONGITUDE] = float(orginalMessage[AdsbMessageField.LONGITUDE])
-        message[AdsbMessageField.GROUND_SPEED] = float(orginalMessage[AdsbMessageField.GROUND_SPEED])
-        message[AdsbMessageField.TRACK] = float(orginalMessage[AdsbMessageField.TRACK])
-        message[AdsbMessageField.VERTICAL_RATE] = float(orginalMessage[AdsbMessageField.VERTICAL_RATE])
-
-        message[AdsbMessageField.ALTITUDE] = float(orginalMessage[AdsbMessageField.ALTITUDE])
-        message[AdsbMessageField.GEO_ALTITUDE] = float(orginalMessage[AdsbMessageField.GEO_ALTITUDE])
-
-        if message[AdsbMessageField.SQUAWK] != "NaN":
-            message[AdsbMessageField.SQUAWK] = int(orginalMessage[AdsbMessageField.SQUAWK])
+        if message[AdsbMessageField.TIMESTAMP] != '':
+            message[AdsbMessageField.TIMESTAMP] = int(originalMessage[AdsbMessageField.TIMESTAMP])
         else:
-            message[AdsbMessageField.SQUAWK] = None
+            message[AdsbMessageField.TIMESTAMP] = np.nan
+        if message[AdsbMessageField.LATITUDE] != '':
+            message[AdsbMessageField.LATITUDE] = float(originalMessage[AdsbMessageField.LATITUDE])
+        else:
+            message[AdsbMessageField.LATITUDE] = np.nan
+        if message[AdsbMessageField.LONGITUDE] != '':
+            message[AdsbMessageField.LONGITUDE] = float(originalMessage[AdsbMessageField.LONGITUDE])
+        else:
+            message[AdsbMessageField.LONGITUDE] = np.nan
+        if message[AdsbMessageField.GROUND_SPEED] != '':
+            message[AdsbMessageField.GROUND_SPEED] = float(originalMessage[AdsbMessageField.GROUND_SPEED])
+        else:
+            message[AdsbMessageField.GROUND_SPEED] = np.nan
+        if message[AdsbMessageField.TRACK] != '':
+            message[AdsbMessageField.TRACK] = float(originalMessage[AdsbMessageField.TRACK])
+        else:
+            message[AdsbMessageField.TRACK] = np.nan
+        if message[AdsbMessageField.VERTICAL_RATE] != '':
+            message[AdsbMessageField.VERTICAL_RATE] = float(originalMessage[AdsbMessageField.VERTICAL_RATE])
+        else:
+            message[AdsbMessageField.VERTICAL_RATE] = np.nan
+        if message[AdsbMessageField.ALTITUDE] != '':
+            message[AdsbMessageField.ALTITUDE] = float(originalMessage[AdsbMessageField.ALTITUDE])
+        else:
+            message[AdsbMessageField.ALTITUDE] = np.nan
+        if message[AdsbMessageField.GEO_ALTITUDE] != '':
+            message[AdsbMessageField.GEO_ALTITUDE] = float(originalMessage[AdsbMessageField.GEO_ALTITUDE])
+        else:
+            message[AdsbMessageField.GEO_ALTITUDE] = np.nan
+
+        if message[AdsbMessageField.SQUAWK] != "NaN" and message[AdsbMessageField.SQUAWK] != "":
+            message[AdsbMessageField.SQUAWK] = int(originalMessage[AdsbMessageField.SQUAWK])
+        else:
+            message[AdsbMessageField.SQUAWK] = np.nan
 
         if message[AdsbMessageField.ON_GROUND] == "True":
             message[AdsbMessageField.ON_GROUND] = True
@@ -62,8 +86,8 @@ def classify_aircraft(body):  # noqa: E501
         labels_flight_1 = probabilityToLabel(predictions[icao])
         major_label_flight_1 = np.bincount(labels_flight_1).argmax()
         truth = getTruthLabelFromIcao(icao)
-
-        return {'message': orginalMessage, 'prediction': labelToName(major_label_flight_1),
+        print(labelToName(major_label_flight_1))
+        return {'message': originalMessage, 'prediction': labelToName(major_label_flight_1),
                 'truth': labelToName(truth)}, 200
     except Exception as e:
-        return {'message': orginalMessage, 'error': f'{e}'}, 500
+        return {'message': originalMessage, 'error': f'{e}'}, 500
